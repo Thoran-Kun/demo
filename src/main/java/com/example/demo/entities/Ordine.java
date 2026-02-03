859b02cd175350db7d1990d461b31df3dcbc6f8a
Salvatore <pepesalvatore12@gmail.com>
2026-02-03 16:17:26 +0100

    add Runner

--- src/main/java/com/example/demo/entities/Ordine.java
+++ src/main/java/com/example/demo/entities/Ordine.java
@@ -1,4 +1,38 @@
 package com.example.demo.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
 public class Ordine {
+    private int numeroOrdine;
+    private int numeroCoperti;
+    private LocalDateTime oraAcquisizione;
+    private List<Menu> elementiOrdine = new ArrayList<>();
+    private Tavolo tavolo;
+    private double costoCoperto;
+    private StatoOrdine statoOrdine;
+
+    public Ordine(int numeroOrdine, int numeroCoperti, LocalDateTime oraAcquisizione, List<Menu> elementiOrdine, Tavolo tavolo, double costoCoperto, StatoOrdine statoOrdine) {
+        this.numeroOrdine = numeroOrdine;
+        this.numeroCoperti = numeroCoperti;
+        this.oraAcquisizione = LocalDateTime.now();
+        this.elementiOrdine = elementiOrdine;
+        this.tavolo = tavolo;
+        this.costoCoperto = costoCoperto;
+        this.statoOrdine = statoOrdine;
+    }
+
+    public double costoTotale() {
+        double prezzoTotale = elementiOrdine.stream()
+                .mapToDouble(elemento -> elemento.getPrice()).sum();
+        return prezzoTotale + (costoCoperto * numeroCoperti);
+    }
 }
---
+++ src/main/java/com/example/demo/runners/MyRunner.java
@@ -0,0 +1,4 @@
+package com.example.demo.runners;
+
+public class MyRunner {
+}
